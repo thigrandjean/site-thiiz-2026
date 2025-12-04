8f5f05969b38c5d68344a25f6fcc9057b81f217d
Thiago Grandjean <thigrandjean@yahoo.com.br>
2025-12-04 15:10:01 -0300

    feat: posts cards

---
+++ .prettierrc.mjs
@@ -0,0 +1,12 @@
+// .prettierrc.mjs
+export default {
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
