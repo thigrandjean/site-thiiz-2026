5d2b51b29511383b74eac3cf807694f183a74d77
Thiago Grandjean <thigrandjean@yahoo.com.br>
2025-12-05 10:25:39 -0300

    feat: add sitemap and config images to cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://thiiz.com.br",
   integrations: [mdx(), sitemap(), icon()],
 
   vite: {
     plugins: [tailwindcss()]
   },
 
-  adapter: cloudflare()
+  adapter: cloudflare({
+    imageService: "compile"
+  })
 });
+
