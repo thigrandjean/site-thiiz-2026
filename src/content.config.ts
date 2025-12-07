0f8b4c711ed0a73674e5e186caef1e1d842fcac3
Thiago Grandjean <thigrandjean@yahoo.com.br>
2025-12-07 11:14:55 -0300

    feat: add post generator and fix slugs

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,15 +1,16 @@
-import { defineCollection, z } from 'astro:content'
+import { defineCollection, z } from "astro:content";
 
-import { glob } from 'astro/loaders'
-
+import { glob } from "astro/loaders";
 
 const posts = defineCollection({
   loader: glob({ pattern: "**/*.mdx", base: "./src/posts/" }),
-  schema: ({ image }) => z.object({
-    title: z.string(),
-    category: z.string(),
-    heroImage: image().optional(),
-  })
-})
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      slug: z.string(),
+      category: z.string(),
+      heroImage: image().optional()
+    })
+});
 
-export const collections = { posts }
+export const collections = { posts };
