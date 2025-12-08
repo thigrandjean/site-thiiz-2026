78df7ac6f75c8d2afceb9bde9ba90cf19646a00a
Thiago Grandjean <thigrandjean@yahoo.com.br>
2025-12-08 09:51:41 -0300

    feat: add umami

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
+import umami from "@yeskunall/astro-umami";
 import tailwindcss from "@tailwindcss/vite";
 
 import icon from "astro-icon";
@@ -12,7 +12,12 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   site: "https://thiiz.com.br",
-  integrations: [mdx(), sitemap(), icon()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    icon(),
+    umami({ id: "dc4fd9b6-fe68-46e4-a1b2-73055732c966" })
+  ],
 
   vite: {
     plugins: [tailwindcss()]
@@ -22,4 +27,3 @@ export default defineConfig({
     imageService: "compile"
   })
 });
-
